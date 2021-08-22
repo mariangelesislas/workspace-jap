fda8c32e4d1219cba85f183de12015a7d7dfbccc
mariangelesislas <mariaangelesislas@gmail.com>
2021-08-22 18:17:06 -0300

    nuevos cambios

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function validarCamposVacios(){
+
+    let usuario = document.getElementById("username").value;
+    let contrasena = document.getElementById("password").value;
+    // si los campos no estan vacios se redirecciona al archivo home
+    if((usuario !=="")&& (contrasena !=="")){
+        document.location.href="home.html";
+    }else
+    {
+        alert("Error, debe completar todos los campos.");
+    }
+}
+
