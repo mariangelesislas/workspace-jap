56048f7919dfbaccfaed518b4104abe80925216e
mariangelesislas <mariaangelesislas@gmail.com>
2021-08-22 19:11:50 -0300

    comiit nuevo

--- js/index.js
+++ js/index.js
@@ -4,9 +4,8 @@ function validarCamposVacios(){
     let contrasena = document.getElementById("password").value;
     // si los campos no estan vacios se redirecciona al archivo home
     if((usuario !=="")&& (contrasena !=="")){
-        window.location.href="home.html";
-    }else
-    {
+      document.location.href = "home.html";
+    }else{
         alert("Error, debe completar todos los campos.");
     }
 }
