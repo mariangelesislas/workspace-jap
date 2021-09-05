41a1365da40e2ea73211349798cebf5dd3a05483
mariangelesislas <mariaangelesislas@gmail.com>
2021-09-05 15:21:27 -0300

    cambios a index e init

--- js/index.js
+++ js/index.js
@@ -1,14 +1,20 @@
+function saveUsername(){
+    var name = document.getElementById('username').value;
+     localStorage.setItem('username', name);
+};
+
+
 function validarCamposVacios(){
 
     let usuario = document.getElementById("username").value;
     let contrasena = document.getElementById("password").value;
     // si los campos no estan vacios se redirecciona al archivo home
-    if((usuario !=="")&& (contrasena !=="")){
-        document.location.href="home.html";
+    if((usuario =="")&& (contrasena =="")){
+        alert("Error, debe completar todos los campos.");
+        
     }else
     {
-        alert("Error, debe completar todos los campos.");
+        saveUsername()
+        document.location.href="products.html";
     }
 }
-
-
