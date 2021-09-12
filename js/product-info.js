7db2579aa2ce19021eea6b6019e849c3da905f7f
mariangelesislas <mariaangelesislas@gmail.com>
2021-09-12 14:57:20 -0300

    funcion comment

--- js/product-info.js
+++ js/product-info.js
@@ -44,14 +44,14 @@ document.addEventListener("DOMContentLoaded", function(e){
     });
 });
 
-var currentCommentsArray = []; 
+var comments = {};
 
-function showComments(){
+function showComments(array){
 
 
     let htmlContentToAppend = "";
-    for(let i = 0; i < currentCommentsArray.length; i++){
-        let comment = currentCommentsArray[i];
+    for(let i = 0; i < array.length; i++){
+        let comment = array[i];
 
             
             
@@ -83,6 +83,7 @@ document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
             
+            comments = resultObj.data;
             showComments();
         }
     });
