249cf50c9842354b37617b121645249392bcd5d2
mariangelesislas <mariaangelesislas@gmail.com>
2021-09-12 15:11:13 -0300

    funcion function

--- js/product-info.js
+++ js/product-info.js
@@ -44,14 +44,14 @@ document.addEventListener("DOMContentLoaded", function(e){
     });
 });
 
-var comments = {};
+var commentArray = [];
 
-function showComments(array){
+function showComments(){
 
 
     let htmlContentToAppend = "";
-    for(let i = 0; i < array.length; i++){
-        let comment = array[i];
+    for(let i = 0; i < commentArray.length; i++){
+        let comment = commentArray[i];
 
             
             
@@ -84,7 +84,7 @@ document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
             
-            comments = resultObj.data;
+            
             showComments();
         }
     });
