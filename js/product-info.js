da96b3cb7e849e1ec46545847ed84505972d3a55
mariangelesislas <mariaangelesislas@gmail.com>
2021-09-14 07:51:26 -0300

    function info sin currency

--- js/product-info.js
+++ js/product-info.js
@@ -32,13 +32,13 @@ document.addEventListener("DOMContentLoaded", function(e){
             let productNameHTML = document.getElementById("productName");
             let productDescriptionHTML = document.getElementById("productDescription");
             let productSoldCountHTML = document.getElementById("productSoldCount");
-            let productCurrencyAndCostHTML = document.getElementById("productCurrencyAndCost")
+            let productCostHTML = document.getElementById("productCurrencyAndCost")
            
             productCategoryHTML.innerHTML = product.category
             productNameHTML.innerHTML = product.name;
             productDescriptionHTML.innerHTML = product.description;
             productSoldCountHTML.innerHTML = product.soldCount;
-            productCurrencyAndCostHTML = product.currency + " " + product.cost;
+            productCostHTML = product.cost;
 
             //Muestro las imagenes en forma de galería
             showProductPictures(product.images);
