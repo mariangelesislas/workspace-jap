1e1cfa06e611be74bc3b3e743de332e6668451f4
mariangelesislas <mariaangelesislas@gmail.com>
2021-09-16 15:34:18 -0300

    new chanfe (function show comments)

--- js/product-info.js
+++ js/product-info.js
@@ -120,7 +120,7 @@ function showComment(){
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCTS_INFO_COMMENTS_URL).then(function(resultObj){
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
             
             showComment();
