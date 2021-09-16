8a6b1ba4179cb02b88eee023b37915b5927c90b6
mariangelesislas <mariaangelesislas@gmail.com>
2021-09-16 16:16:49 -0300

    Update product-info.js

--- js/product-info.js
+++ js/product-info.js
@@ -122,9 +122,9 @@ function showComment(){
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
+            commentArray = resultObj.data;
             
-            
-            showComment(commentArray,resultObj.data);
+            showComment(commentArray);
         }
     });
 });
