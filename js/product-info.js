0df9059e05c83e9b0904c4188225ccb4b96cc5d9
mariangelesislas <mariaangelesislas@gmail.com>
2021-09-18 21:45:33 -0300

    sort for comments score and datetime

--- js/product-info.js
+++ js/product-info.js
@@ -46,46 +46,48 @@ document.addEventListener("DOMContentLoaded", function(e){
     });
 });
 
-/*var commentArray = [];
 
-function showComment(array){
-    
-    let htmlContentToAppend = "";
-    for(let i = 0; i < array.length; i++){
-        let comment = array[i];
 
-        htmlContentToAppend += `
-        <div class="list-group-item list-group-item-action">
-                <div class="row">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h2 class="mb-1">`+ comment.user + " " + "Puntuación" + " " + comment.score + " " + comment.dateTime +`</h2>
-                    </div>
-                    <br>
-                    <div>
-                     <h4 class="mb-1">`+ comment.description +`</h4>
-                    </div>
-                </div>
-        </div>
-        `
-        
-        document.getElementById("comment-list-container").innerHTML = htmlContentToAppend;
-    }
-}
 
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok"){
-            commentArray = resultObj.data;
-            
-            showComment(commentArray);
-        }
-    });
-}); */
 
+const ORDER_ASC_BY_DATE = "Actuales";
+const ORDER_DESC_BY_DATE = "Antiguos";
+const ORDER_BY_MOST_POPULAR = "Mas populares";
+var currentSortCriteriaForComment = undefined;
 var commentArray = [];
+
+function sortComment(criteria, array){
+    let result = [];
+    if (criteria === ORDER_ASC_BY_DATE)
+    {
+        result = array.sort(function(a, b) {
+            if ( a.dateTime > b.dateTime ){ return -1; }
+            if ( a.dateTime < b.dateTime ){ return 1; }
+            return 0;
+        });
+
+      /* funcion que ordena de menor a mayor segun precio*/        
+    }else if (criteria === ORDER_DESC_BY_DATE){
+        result = array.sort(function(a, b) {
+            if ( a.dateTime < b.dateTime ){ return -1; }
+            if ( a.dateTime > b.dateTime ){ return 1; }              
+            return 0;
+        });
+
+     /*funcion que ordena en ordena de mayor a emnor segun relevancia (prductos mas vendidos) */
+    }else if (criteria === ORDER_BY_MOST_POPULAR){
+        result = array.sort(function(a, b) {
+            let aScore= parseInt(a.score);
+            let bScore = parseInt(b.score);
+
+            if ( aScore > bScore ){ return -1; }
+            if ( aScore < bScore ){ return 1; }
+            return 0;
+        });
+    }
+
+    return result;
+}
 
 function showComment(){
 
@@ -115,6 +117,20 @@ function showComment(){
         document.getElementById("comment-list-container").innerHTML = htmlContentToAppend;
     }
 
+    /*funcion que muestra segun el criterio y el array*/
+function sortAndShowComment(commentSortCriteria, commentArray){
+    currentSortCriteriaForComment = commentSortCriteria;
+
+    if(commentArray != undefined){
+        currentCommentArray = commentArray;
+    }
+
+    currentCommentArray = sortComment(currentSortCriteriaForComment, currentCommentArray);
+
+    //Muestro los productos ordenados
+    showComment();
+}
+
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -123,8 +139,23 @@ document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
             commentArray = resultObj.data;
-            
-            showComment(commentArray);
+            sortAndShowComment(ORDER_ASC_BY_DATE, resultObj.data);
         }
     });
+     // nos permite poder darle funcionabilidad al boton
+    document.getElementById("sortAscByDate").addEventListener("click", function(){
+        sortAndShowComment(ORDER_ASC_BY_DATE);
+    });
+
+    document.getElementById("sortDescByDate").addEventListener("click", function(){
+        sortAndShowComment(ORDER_DESC_BY_DATE);
+    });
+
+    document.getElementById("sortByMostPopular").addEventListener("click", function(){
+        sortAndShowComment(ORDER_BY_MOST_POPULAR);
+
+        showComment();
+    });
 });
+            
+          
