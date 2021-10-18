560e3fd80c2f6410f5c0bbd93588b2815ce9e8c2
mariangelesislas <mariaangelesislas@gmail.com>
2021-10-17 21:55:10 -0300

    carrito

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,54 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+var cartList = [];
+let subTotalCart;
 
+
+// funcion que actualzia el subtotal cuando se cambia en valor de la cantidad de productos
+function updateSubtotal(count,unitCost,id,currency) {
+    subTotalCart = count*unitCost;
+    document.getElementById("subtotal"+id).innerHTML = currency + subTotalCart;
+    document.getElementById("grandtotal").innerHTML = currency + subTotalCart; // total 
+    
+
+}
+
+
+// funcion que muestra los datos del carrito
+function showCartList(){
+
+    let htmlContentToAppend = "";
+    let i = 0
+        for(let article of cartList){
+        
+            htmlContentToAppend += `
+            
+            <tr>
+            <td><img src="`+article.src+`" class = "img-fluid" style ="max-width:50px!important"></td>
+            <td class="align-middle">`+article.name+`</td>
+            <td class="align-middle">`+article.currency + " " + article.unitCost +`</td>
+            <td class="align-middle"><input type="number" min ="1" value=`+ article.count+` id ="`+i+`" onchange="updateSubtotal(this.value,`+article.unitCost+`,`+i+`,'`+article.currency+`')"></td> 
+            <td class="align-middle subtotal" id="subtotal`+i+`">`+article.currency + article.count * article.unitCost +`</td>
+
+            </tr>`
+            i++;
+
+            // permite que se vea el total
+             
+            document.getElementById("grandtotal").innerHTML =  article.currency + " " + article.count*article.unitCost;
+            
+            
+        }
+     // muestra en el html la informacion
+    document.getElementById("cart").innerHTML = htmlContentToAppend;
+}
+
+// funcion que obtiene la informacion del json
+
+getJSONData(CART_INFO_URL).then(function(resultObj){
+    if (resultObj.status === "ok"){
+        
+        cartList = resultObj.data.articles;
+        showCartList();
+        
+    }
 });
+
