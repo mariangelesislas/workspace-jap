d9e43a9c6887057e31272e9f433f82511797dc8a
mariangelesislas <mariaangelesislas@gmail.com>
2021-10-31 15:51:24 -0300

    mi perfil js

--- js/my-profile.js
+++ js/my-profile.js
@@ -8,17 +8,28 @@ let getUserSignUpForm={
     celular:document.getElementById("cellphone").value,
     sobrenombre:document.getElementById("nickname").value,
     cumpleaños:document.getElementById("birthday").value,
-    gitHubLink:document.getElementById("usergithub").value
+    gitHubLink:document.getElementById("usergithub").value,
+    correo:document.getElementById("email").value
+   
 
     }
-// permite guardar la informacion de manera correcta asi se puede recuperar
-localStorage.setItem("userprofile",JSON.stringify(getUserSignUpForm));
+    // permite guardar la informacion de manera correcta asi se puede recuperar
+    localStorage.setItem("userprofile",JSON.stringify(getUserSignUpForm));
+
 }
 
 //permite mostrar en pantalla los valores obtenidos anteriormente en su input correspondiente
 function showInputValue(){
     let userProfileAttributeValue= JSON.parse(localStorage.getItem("userprofile"));
     console.log(userProfileAttributeValue.nombre);
+    console.log(userProfileAttributeValue.primerApellido);
+    console.log(userProfileAttributeValue.segundoApellido);
+    console.log(userProfileAttributeValue.edad);
+    console.log(userProfileAttributeValue.celular);
+    console.log(userProfileAttributeValue.sobrenombre);
+    console.log(userProfileAttributeValue.cumpleaños);
+    console.log(userProfileAttributeValue.gitHubLink);
+    console.log(userProfileAttributeValue.correo);
 
 }
 
