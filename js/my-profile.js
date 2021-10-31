8c9d046a7fee1c01ed6c4035328272561fa21bdf
mariangelesislas <mariaangelesislas@gmail.com>
2021-10-31 16:39:20 -0300

    Update my-profile.js

--- js/my-profile.js
+++ js/my-profile.js
@@ -20,7 +20,7 @@ let getUserSignUpForm={
 
 //permite mostrar en pantalla los valores obtenidos anteriormente en su input correspondiente
 function showInputValue(){
-    if ( getUserSignUpForm === null){
+    if ( "userprofile" === null){
     let userProfileAttributeValue= JSON.parse(localStorage.getItem("userprofile"));
     console.log(userProfileAttributeValue.nombre);
     console.log(userProfileAttributeValue.primerApellido);
