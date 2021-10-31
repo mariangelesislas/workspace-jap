f09305f74a4305f716cac1342500ca9765d5f710
mariangelesislas <mariaangelesislas@gmail.com>
2021-10-31 14:17:33 -0300

    mi perfil js

---
+++ js/my-profile.js
@@ -0,0 +1,13 @@
+let getUserSignUpForm={
+    nombre:document.getElementById("name").value,
+    primerApellido:document.getElementById("firstsurname").value,
+    segundoApellido:document.getElementById("secondsurname").value,
+    edad:document.getElementById("userage").value,
+    celular:document.getElementById("cellphone").value,
+    sobrenombre:document.getElementById("nickname").value,
+    cumpleaños:document.getElementById("birthday").value,
+    gitHubLink:document.getElementById("usergithub").value
+
+}
+
+localStorage.setItem("userprofile",getUserSignUpForm);
