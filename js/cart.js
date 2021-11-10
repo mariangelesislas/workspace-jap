38b6ce209ef1031d7f8388d6f64e4de7d7086d82
mariangelesislas <mariaangelesislas@gmail.com>
2021-11-10 10:07:47 -0300

    cart diseño

--- js/cart.js
+++ js/cart.js
@@ -52,3 +52,76 @@ getJSONData(CART_INFO_URL).then(function(resultObj){
     }
 });
 
+//________________________________________________________________
+
+document.getElementById("transferencia-bancaria").addEventListener("change",function(){
+    document.getElementById("numero-de-cuenta").disabled = false;
+
+    //___________________________________________
+
+    document.getElementById("numero-de-tarjeta").disabled = true;
+    document.getElementById("codigo-de-seguridad").disabled = true;
+    document.getElementById("vencimiento").disabled = true;
+    document.getElementById("tarjeta-de-credito").disabled = true;
+
+});
+
+document.getElementById("tarjeta-de-credito").addEventListener("change",function(){
+    document.getElementById("numero-de-tarjeta").disabled = false;
+    document.getElementById("codigo-de-seguridad").disabled = false;
+    document.getElementById("vencimiento").disabled = false;
+    
+    //___________________________________________
+
+    
+    document.getElementById("numero-de-cuenta").disabled = true;
+    document.getElementById("transferencia-bancaria").disabled = true;
+                         
+});
+
+//_________________________________________________
+
+function validateCartDirectionFields(){
+
+    let calle = document.getElementById("street").value;
+    let numerohogar = document.getElementById("housenumber").value;
+    let esquina = document.getElementById("corner").value;
+   
+    if((calle ==="") && (numerohogar ==="") && (esquina ==="")){
+        
+        alert("Error, debe completar todos los campos");
+        
+    }else
+    {
+        alert("¡Su compra ha sido realizada con exito!");
+          
+    }
+  
+
+}
+
+
+
+function validatePaymentMethod(){
+    let numerodetarjeta  = document.getElementById("numero-de-tarjeta").value;
+    let codigodeseguridad = document.getElementById("codigo-de-seguridad").value;
+    let caducidad = document.getElementById("vencimiento").value;
+    let numerodecuenta = document.getElementById("numero-de-cuenta").value;
+
+    if((numerodetarjeta ==="") && (codigodeseguridad ==="") && (caducidad ==="") && (numerodecuenta ==="")){
+        
+        alert("Error, debe completar todos los campos");
+        
+    }else
+    {
+        alert("¡Sus datos han sido guardados");
+    }
+
+}
+
+
+/*
+if ((document.getElementById("tarjeta-de-credito").disabled) && (document.getElementById("transferencia-bancaria").checked) && (numerodecuenta ==="")){
+    alert("Error, debe completar todos los campos");
+}
+*/
