c59fd23be12568965e20f59fdafa2fec45ba11f5
mariangelesislas <mariaangelesislas@gmail.com>
2021-11-11 23:31:19 -0300

    Carrito mejorando funcion total

--- js/cart.js
+++ js/cart.js
@@ -1,13 +1,12 @@
 var cartList = [];
 let subTotalCart;
-
+let totalCart;
 
 // funcion que actualzia el subtotal cuando se cambia en valor de la cantidad de productos
 function updateSubtotal(count,unitCost,id,currency) {
     subTotalCart = count*unitCost;
     document.getElementById("subtotal"+id).innerHTML = currency + subTotalCart;
-    document.getElementById("grandtotal").innerHTML = currency + subTotalCart; // total 
-    
+       
 
 }
 
@@ -34,15 +33,15 @@ function showCartList(){
             <td class="align-middle">`+article.name+`</td>
             <td class="align-middle">`+article.currency + " " + article.unitCost +`</td>
             <td class="align-middle"><input type="number" min ="1" value=`+ article.count+` id ="`+i+`" onchange="updateSubtotal(this.value,`+article.unitCost+`,`+i+`,'`+article.currency+`')"></td> 
-            <td class="align-middle subtotal" id="subtotal`+i+`">`+article.currency + article.count * article.unitCost +`</td>
+            <td class="align-middle subtotal" name="sub" id="subtotal`+i+`">`+article.currency + article.count * article.unitCost +`</td>
 
             </tr>`
             i++;
              
 
-            // permite que se vea el total
+           
              
-            document.getElementById("grandtotal").innerHTML = article.currency + " " + article.count * article.unitCost;
+            
          
            
         }
